9bf6df3c804921f557369ce149dce979272dd483
Pranjal Chourasia <61688724+pranjal5399@users.noreply.github.com>
2020-10-15 16:05:12 +0530

    minor changes

--- backend/main.go
+++ backend/main.go
@@ -25,7 +25,8 @@ type articleHandlers struct {
 	store map[string]Article
 }
 
-func (h *articleHandlers) articles(w http.ResponseWriter, r *http.Request) {
+//CHECKS REQUEST GOING TO /articles/ IS POST OR GET
+func (h *articleHandlers) getAllArticles(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		h.get(w, r)
@@ -41,6 +42,8 @@ func (h *articleHandlers) articles(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//POST ONE ARTICLE (json body is in routes.rest)
+//POST http://localhost:8080/articles
 func (h *articleHandlers) post(w http.ResponseWriter, r *http.Request) {
 	bodyBytes, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
@@ -75,6 +78,8 @@ func (h *articleHandlers) post(w http.ResponseWriter, r *http.Request) {
 	defer h.Unlock()
 }
 
+//GET ALL ARTICLES (with pagination)
+//GET http://localhost:8080/articles?page=2
 func (h *articleHandlers) get(w http.ResponseWriter, r *http.Request) {
 
 	articles := make([]Article, 0, 1000)
@@ -112,7 +117,9 @@ func (h *articleHandlers) get(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonBytes)
 }
 
-func (h *articleHandlers) getarticle(w http.ResponseWriter, r *http.Request) {
+//GET ONE ARTICLE
+//GET http://localhost:8080/articles/1
+func (h *articleHandlers) getOneArticle(w http.ResponseWriter, r *http.Request) {
 
 	parts := strings.Split(r.URL.String(), "/")
 
@@ -144,6 +151,8 @@ func (h *articleHandlers) getarticle(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonBytes)
 }
 
+//SEARCH ARTICLES(case insensitive)
+//GET GET http://localhost:8080/articles/search?q=abc
 func (h *articleHandlers) searcharticle(w http.ResponseWriter, r *http.Request) {
 	articles := make([]Article, 0, 1000)
 	queryURL := strings.Split(r.URL.String(), "?q=")
@@ -190,10 +199,10 @@ func newArticleHandlers() *articleHandlers {
 	return &articleHandlers{
 		store: map[string]Article{
 			"1": Article{
-				Title:    "ABC",
-				Subtitle: "BMW",
+				Title:    "PS4 Firmware Update 8.00 Available to Download Today",
+				Subtitle: "PS4 firmware",
 				ID:       "1",
-				Content:  "kalyani halisahar ranaghat",
+				Content:  "Pre-set avatars are also getting a bit of a facelift with new pictures to choose from. They include profile pictures from PS4 titles such as Bloodborne, Journey, Ghost of Tsushima, The Last of Us: Part II, and Uncharted 4: A Thief’s End. You can check them all out through here.",
 				Creation: "2020-10-14 00:40:32.0093369 +0530 IST m=+222.416472201",
 			},
 		},
@@ -202,9 +211,9 @@ func newArticleHandlers() *articleHandlers {
 
 func main() {
 	articleHandlers := newArticleHandlers()
-	http.HandleFunc("/articles", articleHandlers.articles)
+	http.HandleFunc("/articles", articleHandlers.getAllArticles)
 	http.HandleFunc("/articles/search", articleHandlers.searcharticle)
-	http.HandleFunc("/articles/", articleHandlers.getarticle)
+	http.HandleFunc("/articles/", articleHandlers.getOneArticle)
 	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
